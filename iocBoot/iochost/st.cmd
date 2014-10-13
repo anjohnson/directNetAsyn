bf79aef32fd824014c5548abf2ae584580bc3257
Andrew Johnson <anj@aps.anl.gov>
2014-10-13 17:49:26 -0500

    Change host and port names

--- iocBoot/iochost/st.cmd
+++ iocBoot/iochost/st.cmd
@@ -1,4 +1,4 @@
-#!../../bin/solaris-sparc/dnaTest
+#!../../bin/linux-x86/dnaTest
 < envPaths
 
 cd ${TOP}
@@ -7,22 +7,22 @@ dnaTest_registerRecordDeviceDriver(pdbbase)
 
 # Init asyn remote IP port
 # drvAsynIPPortConfigure 'port name' 'host:port [protocol]' priority 'disable auto-connect' noProcessEos
-drvAsynIPPortConfigure serials8n4-1 serials8n4:4001 0 0 0
+drvAsynIPPortConfigure Moxa01-1 moxa01:4001 0 0 0
 
 # Init asyn local serial port
 # drvAsynSerialPortConfigure 'port name' 'tty name' priority 'disable auto-connect' noProcessEos
 #drvAsynSerialPortConfigure ttya /dev/ttyS0 0 0 0
-#asynSetOption serials8n4-1 0 baud 9600
-#asynSetOption serials8n4-1 0 bits 8
-#asynSetOption serials8n4-1 0 parity odd
+#asynSetOption Moxa01-1 0 baud 9600
+#asynSetOption Moxa01-1 0 bits 8
+#asynSetOption Moxa01-1 0 parity odd
 
 # Create PLC:
 # createDnAsynPLC 'PLC name' 'directNet slave ID' 'Asyn port name'
-createDnAsynPLC test 1 serials8n4-1
+createDnAsynPLC test 1 Moxa01-1
 
 # Debugging:
-#asynSetTraceMask serials8n4-1 0 19
-#asynSetTraceIOMask serials8n4-1 0 4
+#asynSetTraceMask Moxa01-1 0 19
+#asynSetTraceIOMask Moxa01-1 0 4
 #var devDnAsynDebug
 
 dbLoadRecords db/DL250stat.db "name=dnaTest,plc=test"
